c982962d47cef4dd540b51d646c35b3b7df80e61
iamsonalshitole <shitolesonal2001@gmail.com>
2022-01-28 12:29:00 +0530

    Digital Clcok

---
+++ clock.js
@@ -0,0 +1,25 @@
+function displayTime() {
+    var dateTime=new Date();
+    var hr=dateTime.getHours();
+    var min=dateTime.getMinutes();
+    var sec=dateTime.getSeconds();
+    var session=document.getElementById('ampm');
+    document.getElementById('hour').innerHTML=hr;
+    document.getElementById('minute').innerHTML=min;
+    document.getElementById('second').innerHTML=sec;
+
+    
+    
+    if(hr>=12){
+        session.innerHTML='PM';
+        
+       
+    }
+    else{
+        session.innerHTML='AM';
+    }
+
+
+    
+}
+setInterval(displayTime, 10);
